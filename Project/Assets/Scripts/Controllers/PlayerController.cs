f4decf97c26f1dbc7ae495615f6ccb0de11e5722
Aan'allein <67291035+Benayash@users.noreply.github.com>
2020-08-20 07:22:47 +0300

    Joystick and Movement
    Refined the movement of the player, added Joystick and android support and implemented them.

--- Project/Assets/Scripts/Controllers/PlayerController.cs
+++ Project/Assets/Scripts/Controllers/PlayerController.cs
@@ -7,15 +7,18 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public NavMeshAgent agent;
-    public Rigidbody rb;
+    public Joystick joystick;
     public float speed = 6f;
-
+    public float rotationSpeed = 10f;
     public Camera camera;
+
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        rb = GetComponent<Rigidbody>();
         camera = Camera.main;
 
     }
@@ -23,15 +26,49 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookat = new Vector3(0f, Input.mousePosition.y, 0f);
-        Vector3 lookpos = camera.ScreenToWorldPoint(lookat);
-        transform.LookAt(lookat);
 
-        float horInput = Input.GetAxis("Horizontal");
-        float verInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horInput, 0f, verInput);
+        #region Joystick and Movement Controllers
+
+        //gets the movement of the joystick
+        if (joystick.Horizontal >= 0.3f)
+        {
+            horizontalMove = speed;
+        }
+        else if(joystick.Horizontal <= -0.3f)
+        {
+            horizontalMove = -speed;
+        }
+        else
+        {
+            horizontalMove = 0f;
+        }
+
+        if (joystick.Vertical >= 0.3f)
+        {
+            verticalMove = speed;
+        }
+        else if (joystick.Vertical <= -0.3f)
+        {
+            verticalMove = -speed;
+        }
+        else
+        {
+            verticalMove = 0f;
+        }
+
+        //gets the movement and moves the agent
+        Vector3 movement = new Vector3(horizontalMove, 0f, verticalMove);
         Vector3 moveDestination = transform.position + movement;
         agent.destination = moveDestination;
+        if(movement != Vector3.zero)
+        {
+            Vector3 dir = moveDestination - transform.position;
+            dir.y = 0;//This allows the object to only rotate on its y axis
+            Quaternion rot = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, rotationSpeed * Time.deltaTime);
+        }
+
+        #endregion
 
         InventoryOpenClose.Menu(); //Inventory Control
     }
