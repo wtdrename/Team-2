63742fe23ad23b6f0d005af90520663cac96d8eb
Batuhan Özkoç <batur.ozkoc@gmail.com>
2020-08-24 16:03:45 +0300

    Inventory Scripts, Example Medkit, Consumables, Health System added.
    Do not remove Player or Inventory Canvas Prefab, otherwise scripts wont work.

--- Project/Assets/Scripts/Inventory/_Inventory.cs
+++ Project/Assets/Scripts/Inventory/_Inventory.cs
@@ -5,11 +5,58 @@ using UnityEngine;
 public class _Inventory : MonoBehaviour
 {
     public GameObject inventoryPanel;
-    public static _Inventory instance; 
-   
+    public GameObject player;
+    public static _Inventory instance;
+    public List<Item> list = new List<Item>();
+
+    void updatePanelSlots()
+    {
+        int index = 0;
+
+        foreach(Transform child in inventoryPanel.transform)
+        {
+            
+            inventorySlotController slot = child.GetComponent<inventorySlotController>();
+            
+            
+
+            if (index < list.Count)
+            {
+                slot.item = list[index];
+            }
+            else
+            {
+                slot.item = null;
+            }
+
+            slot.updateInfo();
+            index++;
+
+
+        }
+    }
+
+    public void Add(Item item)
+    {
+        if(list.Count < 9)
+        {
+            list.Add(item);
+        }
+        updatePanelSlots();
+    }
+
+    public void Remove(Item item)
+    {
+        list.Remove(item);
+        updatePanelSlots();
+
+    }
+
     void Start()
     {
         instance = this;
+        updatePanelSlots();
+        
     }
 
 
