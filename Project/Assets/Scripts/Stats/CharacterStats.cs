b8a7da3f14ae837a19867447845b8ac33f8696df
Aan'allein <67291035+Benayash@users.noreply.github.com>
2020-08-27 07:11:14 +0300

    Added basic stats

---
+++ Project/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,62 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    #region Defenitions
+
+    public CharacterStats_SO stats;
+
+    #endregion
+
+    #region Increasers
+    public void GiveHealth(int amount)
+    {
+        stats.GiveHealth(amount);
+    }
+    public void GiveShield(int amount)
+    {
+        stats.GiveShield(amount);
+    }
+    public void GiveCredit(int amount)
+    {
+        stats.GiveCredit(amount);
+    }
+    #endregion
+
+    #region Decreasers
+    public void TakeDamage(int amount)
+    {
+        if(stats.currentHealth - amount <= 0)
+        {
+            //dies
+        }
+        else
+        {
+            stats.TakeDamage(amount);
+        }
+    }
+    public void LoseShield(int amount)
+    {
+        int leftOverAmount;
+
+        if (stats.currentShield - amount < 0)
+        {
+            leftOverAmount = amount - stats.currentShield;
+            stats.LoseShield(stats.currentShield);
+            stats.TakeDamage(leftOverAmount);
+        }
+        else
+        {
+            stats.TakeDamage(amount);
+        }
+    }
+    public void TakeCredit(int amount)
+    {
+        stats.TakeCredit(amount);
+        //update inventory event
+    }
+    #endregion
+}
---
+++ Project/Assets/Scripts/Stats/CharacterStats_SO.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Character Stats", menuName ="Character Stats/Stats")]
+public class CharacterStats_SO : ScriptableObject
+{
+
+    #region Defenitions
+
+    public int level = 1;
+    public int currentExp = 0;
+    public int maxExp = 100;
+
+    public int currentHealth;
+    public int maxHealth;
+    public int currentShield;
+    public int maxShield;
+
+    public int baseRange;
+
+    public int baseDamage;
+    public int currentDamage;
+
+    public int currentCredit;
+
+    #endregion
+
+    #region Increasers
+    public void GiveHealth(int amount)
+    {
+        if (currentHealth + amount >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += amount;
+        }
+    }
+    public void GiveShield(int amount)
+    {
+        if (currentShield + amount >= maxShield)
+        {
+            currentShield = maxShield;
+        }
+        else
+        {
+            currentShield += amount;
+        }
+    }
+    public void GiveCredit(int amount)
+    {
+        currentCredit += amount;
+    }
+    #endregion
+
+    #region Decreasers
+    public void TakeDamage(int amount)
+    {
+        if (currentHealth - amount <= 0)
+        {
+            currentHealth = 0;
+            //die
+        }
+        else
+        {
+            currentHealth -= amount;
+        }
+    }
+    public void LoseShield(int amount)
+    {
+        if (currentShield - amount <= 0)
+        {
+            currentShield = 0;
+            TakeDamage(amount - currentShield);
+        }
+        else
+        {
+            currentShield -= amount;
+        }
+    }
+    public void TakeCredit(int amount)
+    {
+        if(currentCredit - amount < 0)
+        {
+            Debug.Log("Not enought Credits");
+            return;
+        }
+        currentCredit -= amount;
+    }
+    #endregion
+}
