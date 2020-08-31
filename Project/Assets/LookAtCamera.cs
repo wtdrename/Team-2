3b90e88378612273cc8de3fa4d86524bee9b88e6
edgesean <43985841+edgesean@users.noreply.github.com>
2020-08-31 13:43:42 +0200

    enemy health bar

---
+++ Project/Assets/LookAtCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+
+    public Transform camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(camera.localPosition + camera.forward);
+        
+    }
+}
