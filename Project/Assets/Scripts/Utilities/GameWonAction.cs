f5e63515105ba87a0f2a1cc2149cc96cb8122c13
R2D2 <66644638+R2rit0@users.noreply.github.com>
2020-12-09 15:57:59 -0400

    Minor Stuff

---
+++ Project/Assets/Scripts/Utilities/GameWonAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Manager
+{
+    public class GameWonAction : MonoBehaviour
+    {
+        /// <summary>
+        /// Add what ever method that should run when the player wins. 
+        /// Todo: Ex. OnEnable() => GameWomAction.gameWon += "methodName";
+        /// It's good practice to add OnDisable() => GameWonAction.gameWon -= "methodName";
+        /// 
+        /// Todo: Use either to call this action.
+        /// GameWon?.Invoke();
+        /// 
+        ///        or
+        /// 
+        /// if(GameWon != Null)
+        ///     GameWon();
+        ///
+        /// Btw it's good practice to check if it's Null.
+        /// </summary>
+        public static Action GameWon;
+    }
+}
